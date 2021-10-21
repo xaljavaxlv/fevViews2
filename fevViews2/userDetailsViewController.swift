b216060714687e37175c1cbffa4cb47d07c7148d
xlv <xaljavamail@gmail.com>
2021-10-21 21:04:34 +0800

    Initial Commit

---
+++ fevViews2/userDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  userDetailsViewController.swift
+//  fevViews2
+//
+//  Created by Vlad Zavada on 21.10.2021.
+//
+
+import UIKit
+
+class userDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
